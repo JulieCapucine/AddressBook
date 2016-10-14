30e9c66a193c88bfa68e7c8fb060662aeb1e0fe9
JulieCapucine <jujucapucine.jp@gmail.com>
2016-10-14 12:59:22 +0200

    menu ++

--- Menu.cpp
+++ Menu.cpp
@@ -13,24 +13,36 @@ void Menu::printMenu(AddressBook addressbook){
 	switch (input){
 		case 0 :
 			addressbook.printAll();
+			std::cout << "####################" << std::endl;
+			this->printMenu(addressbook);
 			break;
 		case 1:
 			addressbook.addContact();
+			std::cout << "####################" << std::endl;
+			this->printMenu(addressbook);
 			break;
 		case 2:
 			addressbook.removeContact();
+			std::cout << "####################" << std::endl;
+			this->printMenu(addressbook);
 			break;
 		case 3:
 			addressbook.search();
+			std::cout << "####################" << std::endl;
+			this->printMenu(addressbook);
 			break;
 		case 4:
 			addressbook.editContact();
+			std::cout << "####################" << std::endl;
+			this->printMenu(addressbook);
 			break;
 		case 5:
 			exit(0);
 			break;
 		default:
 			std::cout << "Oups! No fonction under that number" << std::endl;
+			std::cout << "####################" << std::endl;
+			this->printMenu(addressbook);
 			break;
 	}
 
